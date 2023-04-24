de50081a949ce19e72b5df80b55089de5519f6a6
Denisson Freitas <freitasdenis_dev@hotmail.com>
2023-04-24 16:56:17 -0300

    Alterações no Dockfile

--- TCPClient.py
+++ TCPClient.py
@@ -1,8 +1,7 @@
-import webbrowser
 from socket import *
 
 nomeServidor = '127.0.0.1'
-portaServidor = 12201
+portaServidor = 12000
 
 socketCliente = socket(AF_INET, SOCK_STREAM)
 socketCliente.connect((nomeServidor, portaServidor))
@@ -14,9 +13,7 @@ resposta = socketCliente.recv(4096).decode()
 
 cabecalho, conteudo = resposta.split('\n\n', 1)
 if cabecalho.split()[1] == '200':
-    with open('index.html', 'wb') as arquivo:
-        arquivo.write(conteudo.encode())
-    webbrowser.open_new_tab('http://' + nomeServidor + ':' + str(portaServidor))
+    print(conteudo)
 else:
     print('Erro ao receber a resposta do servidor')
 
